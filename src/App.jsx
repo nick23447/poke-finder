22e68b278119666c6f30220679377ebd6749bbe7
nick23447 <156142540+nick23447@users.noreply.github.com>
2024-10-07 16:30:38 -0700

    copy code from codesandbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Title from "./Title.jsx";
+import Entry from "./Entry.jsx";
+import Info from "./Info.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [name, setName] = useState("");
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    const pokemon = encodeURIComponent(name.toLowerCase());
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}/`;
+    fetch(url)
+      .then((r) => r.json())
+      .then((r) => setData(r))
+      .catch((e) => setData(`${e}`));
+  }, [name]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Pokémon Finder</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <Title text="Poké Finder" />
+      <Entry action={setName} />
+      <Info name={name} data={data} />
+    </div>
+  );
 }
-
-export default App
